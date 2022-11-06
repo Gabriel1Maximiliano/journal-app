3319d3414043880b09731b6496640bae88704866
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-11-06 10:50:20 -0300

    se agrega lógica de registro manual

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,49 +1,75 @@
 
-import React from 'react'
-import {Link as RouterLink} from 'react-router-dom'
-import { Button,Grid, Typography,TextField,Link } from '@mui/material'
-import { Google } from '@mui/icons-material'
-import { AuthLayout } from '../layout/AuthLayout'
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Button,Grid, Typography,TextField,Link } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks/useForm';
+
+const formData = {
+  email:'gabriel@mail.com',
+  password:'123456',
+  displayName:'Gabriel Loza'
+}
 
 export const RegisterPage = () => {
- 
+    
+  const  { displayName, email, password, onInputChange, formState } = useForm( formData );
+
+  const onSubmit = ( event ) =>{
+    event.preventDefault();
+   console.log(formState)
+    
+   };
+
     return (
      
     <AuthLayout title='login' >
-  <form>
+  <form onSubmit={ onSubmit } >
         <Grid container>
-          <Grid item sx={12}>
+          <Grid >
             <TextField 
             label='Nombre completo' 
             type='text'
             placeholder='pepito perez'
             fullWidth
+            name='displayName'
+            value={ displayName }
+            onChange={ onInputChange }
             />
            
             </Grid>
-            <Grid item sx={12}>
+            <Grid >
             <TextField 
             label='email' 
             type='text'
             placeholder='test@mail.com'
             fullWidth
+            name='email'
+            value={ email }
+            onChange={ onInputChange }
             />
              </Grid>
-            <Grid item sx={12} >
+            <Grid  >
             <TextField 
             label='contraseña' 
             type='password'
             placeholder='contraseña'
             fullWidth
+            name='password'
+            value={ password }
+            onChange={ onInputChange }
             />
           </Grid>
           <Grid container spacing={2}  >
            <Grid item>
-            <Button color='error'>
+            <Button 
+            type='submit'
+            color='error'
+            >
              Crear cuenta
             </Button>
            </Grid>
-         
+       
            
            <Grid container direction='row' justifyContent='end'>
             <Typography>¿Ya tenés cuenta?</Typography>
